0b5bcfd1659a41bbbeb098ceb102507f83bb284f
Alian <dolordecorus@gmail.com>
2012-10-24 20:00:31 -0600

    Changed C directory structure

---
+++ CPP/ProtoDebugger/src/protodebugger/model/descriptors/BooleanFieldDescriptorContainer.h
@@ -0,0 +1,36 @@
+#ifndef BOOLEANFIELDDESCRIPTORCONTAINER_H
+#define BOOLEANFIELDDESCRIPTORCONTAINER_H
+
+#include <QtGui/QWidget>
+#include <QtGui/QCheckBox>
+#include <QString>
+#include <google/protobuf/descriptor.h>
+#include <google/protobuf/dynamic_message.h>
+#include "FieldDescriptorContainer.h"
+
+//Forward Declarations
+template<class Object> class FieldDescriptorContainer;
+
+class BooleanFieldDescriptorContainer : public FieldDescriptorContainer<class Object>
+{
+    private:
+        QCheckBox * m_check;
+
+    public:
+        BooleanFieldDescriptorContainer(google::protobuf::FieldDescriptor * field):FieldDescriptorContainer<Object>(field){}
+       ~BooleanFieldDescriptorContainer()
+        {
+            delete m_field;
+            delete &m_value;
+            delete &m_defaultValue;
+        }
+
+        virtual QWidget * getWidget(QWidget * parent = 0);
+        virtual QWidget * getParent();
+        virtual Object * getValue();
+        virtual void setValue(Object * value);
+        virtual QString toString();
+        void buildMsg(google::protobuf::DynamicMessageFactory * dynMsg);
+};
+
+#endif // BOOLEANFIELDDESCRIPTORCONTAINER_H
