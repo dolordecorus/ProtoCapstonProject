684e58c2fa6d10553504adca2788fdb2341bc16a
Alian <dolordecorus@gmail.com>
2012-10-26 17:28:30 -0600

    MOre stuff

--- CPP/ProtoDebugger/src/protodebugger/util/ParseProtoMessage.h
+++ CPP/ProtoDebugger/src/protodebugger/util/ParseProtoMessage.h
@@ -3,8 +3,17 @@
 
 class ParseProtoMessage
 {
-public:
-    ParseProtoMessage();
+    private:
+        ParseProtoMessage();
+        ParseProtoMessage(ParseProtoMessage const&);              // Don't Implement.
+        void operator=(ParseProtoMessage const&); // Don't implement
+
+    public:
+        static ParseProtoMessage& getInstance()
+        {
+            static ParseProtoMessage instance;
+            return instance;
+        }
 };
 
 #endif // PARSEPROTOMESSAGE_H
